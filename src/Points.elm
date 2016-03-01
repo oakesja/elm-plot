abeb1ffee87739ac32c37fb18d51d74bccb7eac7
Jacob Oakes <joakes@indigobio.com>
2016-03-01 10:12:49 -0500

    Added interpolated line and points model

--- src/Points.elm
+++ src/Points.elm
@@ -1,14 +1,14 @@
 module Points where
 
-import Private.Models exposing (Points, InterpolatedPoint, PointValue)
+import Private.Models exposing (Points, InterpolatedPoints, PointValue)
 import Scale.Scale exposing (Scale)
 import Point
 import Svg exposing (Svg, rect)
 
-interpolate : Scale x a -> Scale y b -> Points a b -> List (InterpolatedPoint a b)
+interpolate : Scale x a -> Scale y b -> Points a b -> InterpolatedPoints a b
 interpolate xScale yScale points =
   List.map (Point.interpolate xScale yScale) points
 
-toSvg : (Float -> Float -> Svg) -> List (InterpolatedPoint a b) -> List Svg
+toSvg : (Float -> Float -> Svg) -> InterpolatedPoints a b -> List Svg
 toSvg pointToSvg points =
   List.map (\p -> pointToSvg p.x.value p.y.value) points
